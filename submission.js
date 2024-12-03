b173fd1321cd16ec971423330ba8349f287313e7
Teran <teranarthur99@gmail.com>
2024-12-02 17:09:50 -0700

    Update submission.js

--- submission.js
+++ submission.js
@@ -1,7 +1,31 @@
 const findSum = function(array) {
-    // your code here - don't forget to return a number!
     return array.reduce((accumulator, currentValue) => {
         return accumulator + currentValue;
     }, 0);
-  };
+};
+
+const findFrequency = function(array) {
+  const items = array.join('').toLowerCase().match(/[a-z]/g);
+  const frequencyCounter = {};
+
+  items.forEach(item => {
+    frequencyCounter[item] = (frequencyCounter[item] || 0) + 1;
+  });
+
+  let mostFrequentItem = '';
+  for (const item of Object.keys(frequencyCounter)) {
+    if (frequencyCounter[item] > (frequencyCounter[mostFrequentItem] || 0)) {
+      mostFrequentItem = item;
+    }
+  }
+
+  let leastFrequentItem = '';
+  for (const item of Object.keys(frequencyCounter)) {
+    if (frequencyCounter[item] < (frequencyCounter[leastFrequentItem] || Infinity)) {
+      leastFrequentItem = item;
+    }
+  }
+
+  return { most: mostFrequentItem, least: leastFrequentItem };
+}
 
